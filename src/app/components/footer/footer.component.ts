d267c0fc154028d54fd50e1ad8a36aa7d088ad7a
NavidFarahmand <NavidFarahmand75@gmail.com>
2019-02-24 04:46:32 +0330

    finished footer content

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -6,8 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
-
-  constructor() { }
+  footNote:string;
+  copyRight:string;
+  constructor() {
+    this.footNote="متن زیرنویس"
+    this.copyRight="متن کپی رایت"
+   }
 
   ngOnInit() {
   }
