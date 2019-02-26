37577d4aa5e257407e0a03c1b5e730a61a035e67
NavidFarahmand <NavidFarahmand75@gmail.com>
2019-02-26 10:44:07 +0330

    add basic contact us content

--- src/app/components/contact-us/contact-us.component.ts
+++ src/app/components/contact-us/contact-us.component.ts
@@ -6,8 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact-us.component.css']
 })
 export class ContactUsComponent implements OnInit {
-
-  constructor() { }
+  contactUsFields:any[];
+  contactUsText:string;
+  constructor() {
+    this.contactUsText="متن ارتباط با ما"
+    this.contactUsFields=[
+      {name:"name",placeholder:"نام و نام خانوادگی",type:"text"},
+      {name:"email",placeholder:"ایمیل",type:"text"},
+      {name:"phone",placeholder:"شماره تلفن",type:"text"},
+      {name:"subject",placeholder:"موضوع",type:"text"},
+      {name:"text",placeholder:"متن پیام",type:"textarea"},
+    ];
+  }
 
   ngOnInit() {
   }
