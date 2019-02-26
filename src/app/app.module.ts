8d9eb201e5e0a2edd348258196b8e9db8f4b1836
NavidFarahmand <NavidFarahmand75@gmail.com>
2019-02-26 06:33:13 +0330

    add contact-us component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HomePageComponent } from './components/home-page/home-page.component';
+import { ContactUsComponent } from './components/contact-us/contact-us.component';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { HomePageComponent } from './components/home-page/home-page.component';
     HeaderComponent,
     FooterComponent,
     HomePageComponent,
+    ContactUsComponent,
   ],
   imports: [
     BrowserModule,
