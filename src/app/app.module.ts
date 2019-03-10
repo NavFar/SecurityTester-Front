92eb4b18ca5e132cf0b93d83e76e22e6c5a8343d
NavidFarahmand <NavidFarahmand75@gmail.com>
2019-03-10 17:57:38 +0330

    add breadcrum

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { FooterComponent } from './components/footer/footer.component';
 import { HomePageComponent } from './components/home-page/home-page.component';
 import { ContactUsComponent } from './components/contact-us/contact-us.component';
 import { LoginComponent } from './components/login/login.component';
+import { SideBreadcrumbComponent } from './side-breadcrumb/side-breadcrumb.component';
 
 @NgModule({
   declarations: [
@@ -18,6 +19,7 @@ import { LoginComponent } from './components/login/login.component';
     HomePageComponent,
     ContactUsComponent,
     LoginComponent,
+    SideBreadcrumbComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/side-breadcrumb/side-breadcrumb.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-side-breadcrumb',
+  templateUrl: './side-breadcrumb.component.html',
+  styleUrls: ['./side-breadcrumb.component.css']
+})
+export class SideBreadcrumbComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
