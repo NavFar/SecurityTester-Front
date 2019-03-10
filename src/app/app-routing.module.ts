44eb432f7c8487b034701ef0efbb6074389ba676
NavidFarahmand <NavidFarahmand75@gmail.com>
2019-03-10 13:15:15 +0330

    add login route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {HomePageComponent} from './components/home-page/home-page.component';
 import {ContactUsComponent} from './components/contact-us/contact-us.component';
-
+import {LoginComponent} from './components/login/login.component';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomePageComponent, pathMatch: 'full' },
   { path: 'contactus', component: ContactUsComponent, pathMatch: 'full' },
+  { path: 'login', component: LoginComponent, pathMatch: 'full' },
+  { path: '**', redirectTo: 'home', pathMatch: 'full' },
+
+
 ];
 
 @NgModule({
