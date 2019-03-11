d6fffac1b455dda266871315340f47a29c4dc6b7
NavidFarahmand <NavidFarahmand75@gmail.com>
2019-03-11 03:58:54 +0330

    add second router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,20 @@ import { Routes, RouterModule } from '@angular/router';
 import {HomePageComponent} from './components/home-page/home-page.component';
 import {ContactUsComponent} from './components/contact-us/contact-us.component';
 import {LoginComponent} from './components/login/login.component';
+import {AdminBaseComponent} from './components/admin-base/admin-base.component';
+import {DashboardComponent} from './components/dashboard/dashboard.component';
+
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomePageComponent, pathMatch: 'full' },
   { path: 'contactus', component: ContactUsComponent, pathMatch: 'full' },
   { path: 'login', component: LoginComponent, pathMatch: 'full' },
+  { path: 'admin', component: AdminBaseComponent,children:[
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    { path: 'dashboard', component: DashboardComponent, pathMatch: 'full', outlet:'adminRouter' },
+  ] },
+  ////////////////////////////////////////////////////////////////////////////////////////////////////////////
   { path: '**', redirectTo: 'home', pathMatch: 'full' },
-
-
 ];
 
 @NgModule({
