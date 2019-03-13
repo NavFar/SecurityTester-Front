3609ce2e559d0cb88455610d04dd48935bf2d94f
NavidFarahmand <NavidFarahmand75@gmail.com>
2019-03-13 16:12:13 +0330

    add motoEdit route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,6 +9,7 @@ import { SiteOptionsDashboardComponent } from './components/site-options-dashboa
 import { UploaderComponent } from './components/uploader/uploader.component';
 import { ReportsOptionsComponent } from './components/reports-options/reports-options.component';
 import { TitleEditComponent } from './components/title-edit/title-edit.component';
+import { MotoEditComponent } from './components/moto-edit/moto-edit.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
@@ -19,6 +20,7 @@ const routes: Routes = [
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////
     { path: 'firstPageDashboard', component: FirstPageDashboardComponent,   outlet:'adminRouter'},
     { path: 'titleEdit', component: TitleEditComponent, pathMatch: 'full', outlet:'adminRouter' },
+    { path: 'motoEdit', component: MotoEditComponent, pathMatch: 'full', outlet:'adminRouter' },
     { path: 'siteOptionsDashboard', component: SiteOptionsDashboardComponent, pathMatch: 'full', outlet:'adminRouter' },
     { path: 'reportsDashboard', component: ReportsOptionsComponent, pathMatch: 'full', outlet:'adminRouter' },
     { path: 'uploader', component: UploaderComponent, pathMatch: 'full', outlet:'adminRouter' },
