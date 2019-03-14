07320e445c025ec749310ccd63d7d3ac6fb1822c
NavidFarahmand <NavidFarahmand75@gmail.com>
2019-03-14 14:24:44 +0330

    finish user component

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-user',
@@ -6,8 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit {
-
-  constructor() { }
+  logout = faSignOutAlt;
+  username:string;
+  constructor() {
+    this.username="نام کاربر"
+   }
 
   ngOnInit() {
   }
