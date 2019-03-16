12668c728a27b0dd5d86be092f7329b7dcc4bc6d
NavidFarahmand <NavidFarahmand75@gmail.com>
2019-03-16 05:08:07 +0330

    finish faq

--- src/app/components/faq/faq.component.ts
+++ src/app/components/faq/faq.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component , OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-faq',
@@ -6,8 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./faq.component.css']
 })
 export class FaqComponent implements OnInit {
-
-  constructor() { }
+  faqs:any[];
+  constructor() {
+    this.faqs=[
+      // {title:"",question:"",answer:""},
+    ];
+   }
 
   ngOnInit() {
   }
