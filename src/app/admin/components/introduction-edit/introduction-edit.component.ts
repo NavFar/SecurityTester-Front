de067cebbc91cd53f2a4d3836d79cab74fbb6c80
NavidFarahmand <NavidFarahmand75@gmail.com>
2019-03-18 03:03:56 +0330

    connect to server

--- src/app/admin/components/introduction-edit/introduction-edit.component.ts
+++ src/app/admin/components/introduction-edit/introduction-edit.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { AdminApiService } from  '../../services/admin-api/admin-api.service';
 
 @Component({
   selector: 'admin-introduction-edit',
@@ -10,12 +12,37 @@ export class IntroductionEditComponent implements OnInit {
   introductionEdit:string;
   submitButton:string;
   notShowIntroduction:string;
-  constructor() {
+  content:string;
+  show:boolean;
+  constructor(private adminApi:AdminApiService ,private toast :ToastrService ) {
     this.introductionEdit="ویرایش متن معرفی سایت";
     this.submitButton = "اعمال تغییرات";
     this.notShowIntroduction="عدم نمایش متن معرفی سایت";
+    this.content="",
+    this.show=false;
+    this.toast.info("در حال دریافت اطلاعات");
    }
-  ngOnInit() {
+   ngOnInit() {
+     this.adminApi.getIntroduction().subscribe(
+       (res)=>{
+         this.toast.success("اطلاعات دریافت شد");
+         this.show=res.show;
+         this.content=res.content;
+       },
+       (err)=>{
+         this.toast.error("عدم برقراری ارتباط");
+       }
+     );
+   }
+  submit(){
+    this.toast.info("در حال ثبت داده");
+    this.adminApi.setIntroduction({introduction:{show:this.show,content:this.content}}).subscribe(
+      (res)=>{
+        this.toast.success("اطلاعات ثبت شد");
+      },
+      (err)=>{
+        this.toast.error("عدم برقراری ارتباط");
+      }
+    );
   }
-
 }
