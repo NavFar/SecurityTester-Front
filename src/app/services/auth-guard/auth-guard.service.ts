2de33229d1fb9cc124b7874cbed5b6fcf4db116c
NavidFarahmand <NavidFarahmand75@gmail.com>
2019-03-17 15:13:45 +0330

    fix a typo

--- src/app/services/auth-guard/auth-guard.service.ts
+++ src/app/services/auth-guard/auth-guard.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
-
+import { Router, CanActivate } from '@angular/router';
+import { AuthService } from '../auth/auth.service';
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuardService {
+export class AuthGuardService implements CanActivate {
 
-  constructor() { }
+  constructor(public auth:AuthService , public router:Router) {
+   }
+   canActivate():boolean{
+     let flag:boolean = this.auth.isAuthenticated();
+     if(flag)
+     {
+       this.router.navigate(['login']);
+       return false;
+     }
+     return true;
+   }
 }
