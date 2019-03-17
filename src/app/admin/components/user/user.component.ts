70d231d1b2b0aa55ef865ec117114da337c839f4
NavidFarahmand <NavidFarahmand75@gmail.com>
2019-03-18 00:47:49 +0330

    connect user to api

--- src/app/admin/components/user/user.component.ts
+++ src/app/admin/components/user/user.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
-import { Router }from '@angular/router';
-
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { AdminApiService } from  '../../services/admin-api/admin-api.service';
 @Component({
   selector: 'admin-user',
   templateUrl: './user.component.html',
@@ -10,14 +11,21 @@ import { Router }from '@angular/router';
 export class UserComponent implements OnInit {
   logout = faSignOutAlt;
   username:string;
-  constructor(private router:Router) {
+  constructor(private router:Router ,private adminApi:AdminApiService ,private toast :ToastrService ) {
     this.username="نام کاربر"
    }
 
   ngOnInit() {
+    this.adminApi.getUsername().subscribe(
+      (res)=>{
+        this.username = res;
+      },
+      (err)=>{
+        this.toast.error("عدم برقراری ارتباط");
+      }
+    );
   }
   exit(){
-    console.log("dont")
     localStorage.setItem('token', "");
     this.router.navigate(['login'])
   }
