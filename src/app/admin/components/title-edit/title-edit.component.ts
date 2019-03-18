5d2f9a569aeba6db850e584f4f69865c1a4ebb44
NavidFarahmand <NavidFarahmand75@gmail.com>
2019-03-18 03:37:52 +0330

    connect to server

--- src/app/admin/components/title-edit/title-edit.component.ts
+++ src/app/admin/components/title-edit/title-edit.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { AdminApiService } from  '../../services/admin-api/admin-api.service';
+
 
 @Component({
   selector: 'admin-title-edit',
@@ -8,12 +11,36 @@ import { Component, OnInit } from '@angular/core';
 export class TitleEditComponent implements OnInit {
   titleEdit:string;
   submitButton:string;
-  constructor() {
+  content:string;
+
+    constructor(private adminApi:AdminApiService ,private toast :ToastrService ) {
     this.titleEdit="ویرایش عنوان سایت";
-    this.submitButton = "اعمال تغییرات"
+    this.submitButton = "اعمال تغییرات";
+    this.content="";
+    this.toast.info("در حال دریافت اطلاعات");
    }
 
-  ngOnInit() {
-  }
+   ngOnInit() {
+     this.adminApi.getTitle().subscribe(
+       (res)=>{
+         this.toast.success("اطلاعات دریافت شد");
+         this.content=res.content;
+       },
+       (err)=>{
+         this.toast.error("عدم برقراری ارتباط");
+       }
+     );
+   }
+   submit(){
+     this.toast.info("در حال ثبت داده");
+     this.adminApi.setTitle({title:{content:this.content}}).subscribe(
+       (res)=>{
+         this.toast.success("اطلاعات ثبت شد");
+       },
+       (err)=>{
+         this.toast.error("عدم برقراری ارتباط");
+       }
+     );
+   }
 
 }
