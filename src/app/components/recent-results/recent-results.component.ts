b4a719c6da56907924da7348f0024fb6af2e84b1
NavidFarahmand <NavidFarahmand75@gmail.com>
2019-03-25 22:51:05 +0430

    finished recent component

--- src/app/components/recent-results/recent-results.component.ts
+++ src/app/components/recent-results/recent-results.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { PageContentService } from '../../services/page-content/page-content.service'
+import { Router } from '@angular/router';
+import { ResultService } from '../../services/result/result.service'
 
 @Component({
   selector: 'app-recent-results',
@@ -6,10 +9,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recent-results.component.css']
 })
 export class RecentResultsComponent implements OnInit {
-
-  constructor() { }
+  recentResults:string;
+  targetUrl:string;
+  testDate:string;
+  results;
+  constructor(private router:Router,private pageContentService : PageContentService,private resultService:ResultService) { }
 
   ngOnInit() {
+    this.pageContentService.getPageContent().subscribe(
+      (res) =>{
+        this.targetUrl=res.targetUrl;
+        this.testDate=res.testDate;
+        this.recentResults=res.recentResults;
+      },
+      (err) =>{
+        console.log("cant get values sorry");
+      }
+    );
+    this.resultService.getRecentResults().subscribe(
+      (res)=>{
+        this.results = res;
+      },
+      (err)=>{
+        console.log("cant get values sorry");
+      }
+    );
+  }
+  click(id){
+    this.router.navigate(['/result',id]);
   }
 
 }
